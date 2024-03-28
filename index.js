8376785290761bfb1f003ade7e9f05a3cc73f1f6
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-03-28 20:02:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 // if player 1 wins 
 if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Play 1 Wins!";
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
 else if(randomNumber2 > randomNumber1) {
-    document.querySelector("h1").innerHTML = "Player 2 wins1";
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
 
 }
 else{
